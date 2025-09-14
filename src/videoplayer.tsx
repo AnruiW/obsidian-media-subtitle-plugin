965f71c78ec6f04a54a679904dd4918b524e5764
anrui.wang <anrui.wang@advancegroup.com>
2025-09-14 11:53:12 +0800

    fix react import

--- src/videoplayer.tsx
+++ src/videoplayer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Subtitle } from "src/subtitle";
 import { Play, Pause } from 'lucide-react';
 
